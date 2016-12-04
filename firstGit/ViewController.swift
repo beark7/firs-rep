dddde94e3e580942f42743f6ffbafabb74e64f0a
Pasquale Schioppa <pschioppa92@gmail.com>
2016-12-04 19:06:27 +0100

    testing

--- firstGit/ViewController.swift
+++ firstGit/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("i'm testing git")
+        print("i'm testing git, modified")
     }
 
     override func didReceiveMemoryWarning() {
